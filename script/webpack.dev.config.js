94845953ff5bec2674a19bf5aa68b68a1958682a
chenshenhai <cshenhai@gmail.com>
2020-06-26 17:38:57 +0800

    feat: add webpack-dev-server

--- script/webpack.dev.config.js
+++ script/webpack.dev.config.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = 'development';
 
+const path = require('path');
 const merge = require('webpack-merge');
 const config = require('./webpack.base.config');
 
@@ -7,5 +8,10 @@ const config = require('./webpack.base.config');
 
 module.exports = merge(config, {
   mode: 'development',
-  devtool: '#source-map'
+  devtool: '#source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '..'),
+    compress: true,
+    port: 9000
+  }
 });
