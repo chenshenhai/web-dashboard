3e16f2dcc98b72562f499b33be85ec2b542dff4e
chenshenhai <cshenhai@gmail.com>
2020-06-27 10:37:05 +0800

    feat: update

--- script/webpack.base.config.js
+++ script/webpack.base.config.js
@@ -1,15 +1,16 @@
-const path = require('path');
+// const path = require('path');
 const { createWebpackConfig, srcResolve, distResolve } = require('./common');
-const merge = require('webpack-merge');
 const config = require('./../src/config.json');
 
 const depsKeys = Object.keys(config.deps);
 const pageKeys = Object.keys(config.pages);
-
+const layoutKeys = Object.keys(config.layouts);
 
 const depsExternals = {};
 const depsEntry = {};
 const pagesEntry = {};
+const layoutsEntry = {};
+
 depsKeys.forEach((key) => {
   depsExternals[`${key}`] = `_$WebDashboard$_deps_${config.deps[key]}}`;
   depsEntry[`dep_${config.deps[key]}`] = srcResolve(`dep/${key}.js`);
@@ -17,13 +18,23 @@ depsKeys.forEach((key) => {
 pageKeys.forEach(key => {
   pagesEntry[`page_${config.pages[key]}`] = srcResolve(`page/${key}/index.tsx`);
 })
+layoutKeys.forEach(key => {
+  layoutsEntry[`layout_${config.layouts[key]}`] = srcResolve(`layout/${key}/index.tsx`);
+})
 
 
 module.exports = [
   createWebpackConfig({
     entry: {
+      'main': srcResolve('main.ts'),
+    },
+    externals: depsExternals,
+  }),
+  createWebpackConfig({
+    entry: {
       ...depsEntry,
-      ...pagesEntry, 
+      ...layoutsEntry,
+      ...pagesEntry,
     },
     externals: depsExternals,
   }),
